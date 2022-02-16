f10a6bfec1f298a01b0336b4a597807ef7b8f343
João Távora <joaotavora@gmail.com>
2022-02-16 22:34:00 +0000

    This variation also fails

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
+const virtualId = "@foo.js"
 import path from 'path';
 export default {
-    plugins: [
-        {
-            enforce: 'pre',
-            async resolveId(id) {
-                let [file, query] = id.split('?');
-                const probe = query && query.match(/xoption=([^&]+)/)
-                const x = probe && Number(probe[1])
-                if (x) return {id: path.resolve(file), meta: {x}}
-            },
-            async transform(blob, id) {
-                const x = this.getModuleInfo(id)?.meta?.x
-                if (x) return `window.THE_X = ${x}\n` + blob;
-            }
+  plugins: [
+    {
+      enforce: 'pre',
+      async resolveId(id) {
+        let [prequery, query] = id.split('?');
+        if (prequery !== virtualId) return;
+        const probe = query && query.match(/xoption=([^&]+)/)
+        const x = probe && Number(probe[1])
+        if (x) {
+          return {id: prequery, meta: {x}}
         }
-    ]
+      },
+      async load(id) {
+        if (id === virtualId) {
+          const x = this.getModuleInfo(id)?.meta?.x
+          if (!x) throw new Error('OOPS')
+          return `export default function () {`+
+            `return 21 + ${this.getModuleInfo(id)?.meta?.x};`+
+            `}`
+        }
+      }
+    }
+  ]
 }
